ca2a13f60833ed08f60967b120ad05938cb28986
chongoctoan <102847557+chongoctoan@users.noreply.github.com>
2024-07-08 20:30:18 +0700

    Delete SpringExam directory

--- SpringExam/src/main/java/org/example/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository<Employee> extends JpaRepository<Employee, Long> {
-
-}
-
